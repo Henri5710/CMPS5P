8b52b8f580fc97132bd9944db75a2d126ea30e9f
Henri Geneste <Henri@Henris-MacBook-Pro.local>
2019-02-18 13:54:07 -0800

    most frequent method complete

--- hw5.py
+++ hw5.py
@@ -62,10 +62,102 @@ def password_check(password):           # DO NOT CHANGE THIS LINE
 		result = False
 		#print("8:",result)
 	return result                         # DO NOT CHANGE THIS LINE 
+# 
+# Only use this space to write helper functions (if necessary)
+#
+if __name__ == "__main__":              # DO NOT CHANGE THIS LINE 
+  print(password_check('a12#8'))        # DO NOT CHANGE THIS LINE 
+  print(password_check('a1234567#?'))   # DO NOT CHANGE THIS LINE 
+  print(password_check('abcdefgh#'))    # DO NOT CHANGE THIS LINE 
+  print(password_check('a12345678'))    # DO NOT CHANGE THIS LINE 
+  print(password_check('12345678#'))    # DO NOT CHANGE THIS LINE 
+  print(password_check('abbbcd1@'))     # DO NOT CHANGE THIS LINE 
+  print(password_check('a1@a1@1@'))     # DO NOT CHANGE THIS LINE 
+  print(password_check('a123456#7'))    # DO NOT CHANGE THIS LINE 
+#                          
+# NO CODE HERE
+#
+# QUESTION 2
+#  
+# NO CODE HERE
+#
+def word_counter(file_name):            # DO NOT CHANGE THIS LINE 
+	result = 0                            # DO NOT CHANGE THIS LINE 
+	file = open(file_name,'r')
+	file_lines = file.readlines()
+	file.close()
+	words = []
+	for i in range (0,len(file_lines)):
+		words.append(file_lines[i].split())
+	flat_words = []
+	for sub in words:
+		for i in sub:
+			flat_words.append(i)
+	print(flat_words)
+	result  = len(flat_words)
 
-
+	return result                         # DO NOT CHANGE THIS LINE 
+#                          
+# NO CODE HERE
+#
+if __name__ == "__main__":              # DO NOT CHANGE THIS LINE 
+  print(word_counter('input1.txt'))     # DO NOT CHANGE THIS LINE 
+  print(word_counter('input2.txt'))     # DO NOT CHANGE THIS LINE 
+  print(word_counter('input3.txt'))     # DO NOT CHANGE THIS LINE 
+  print(word_counter('input4.txt'))     # DO NOT CHANGE THIS LINE 
+#                          
+# NO CODE HERE
+#
+def most_frequent(file_name):           # DO NOT CHANGE THIS LINE 
+	result1 = ''                          # DO NOT CHANGE THIS LINE 
+	result2 = 0                           # DO NOT CHANGE THIS LINE 
+	file = open(file_name,'r')
+	file_lines = file.readlines()
+	file.close()
+	words = []
+	for i in range (0,len(file_lines)):
+		words.append(file_lines[i].split())
+	flat_words = []
+	for sub in words:
+		for i in sub:
+			flat_words.append(i)
+	flat_words = [c.lower() for c in flat_words]
+	mydict = {}
+	keys = []
+	values = []
+	x = 0
+	while x < len(flat_words):
+		occurence = 1
+		y = x + 1
+		while y < len(flat_words):
+			if flat_words[y] == flat_words[x]:
+				occurence = occurence + 1
+				del(flat_words[y])
+			else:	
+				y = y + 1
+		keys.append(flat_words[x])
+		values.append(occurence)
+		x = x + 1
+	item = 0
+	while item < len(keys):
+		mydict.update({keys[item] : values[item]})
+		item += 1
+	result1 = max(mydict, key=mydict.get)
+	result2 = max(mydict.values())
+	return result1, result2               # DO NOT CHANGE THIS LINE 
+#                          
+# NO CODE HERE
+#
+if __name__ == "__main__":              # DO NOT CHANGE THIS LINE 
+  print(most_frequent('input1.txt'))    # DO NOT CHANGE THIS LINE 
+  print(most_frequent('input2.txt'))    # DO NOT CHANGE THIS LINE 
+  print(most_frequent('input3.txt'))    # DO NOT CHANGE THIS LINE 
+  print(most_frequent('input4.txt'))    # DO NOT CHANGE THIS LINE 
+#                          
+# NO CODE HERE
+#
 '''
-#OLD VERSION(INCLUDE ALPHA,NUM,SPECIAL BOOLS TO USE)
+#OLD VERSION OF PASSWORD CHECK(INCLUDE ALPHA,NUM,SPECIAL BOOLS TO USE)
 	if len(password) < 8:
 		length = False
 	else:
@@ -135,69 +227,3 @@ def password_check(password):           # DO NOT CHANGE THIS LINE
 	print('Is a character never repeated more than twice consecutively:', no_repeat)
 	print('Is the number of distinct characters at least half of the password length:', distinct)
 	'''
-
-
-# 
-# Only use this space to write helper functions (if necessary)
-#
-if __name__ == "__main__":              # DO NOT CHANGE THIS LINE 
-  print(password_check('a12#8'))        # DO NOT CHANGE THIS LINE 
-  print(password_check('a1234567#?'))   # DO NOT CHANGE THIS LINE 
-  print(password_check('abcdefgh#'))    # DO NOT CHANGE THIS LINE 
-  print(password_check('a12345678'))    # DO NOT CHANGE THIS LINE 
-  print(password_check('12345678#'))    # DO NOT CHANGE THIS LINE 
-  print(password_check('abbbcd1@'))     # DO NOT CHANGE THIS LINE 
-  print(password_check('a1@a1@1@'))     # DO NOT CHANGE THIS LINE 
-  print(password_check('a123456#7'))    # DO NOT CHANGE THIS LINE 
-#                          
-# NO CODE HERE
-#
-# QUESTION 2
-#  
-# NO CODE HERE
-#
-def word_counter(file_name):            # DO NOT CHANGE THIS LINE 
-	result = 0                            # DO NOT CHANGE THIS LINE 
-	file = open(file_name,'r')
-	file_lines = file.readlines()
-	file.close()
-	words = []
-	for i in range (0,len(file_lines)):
-		words.append(file_lines[i].split())
-	flat_words = []
-	for sub in words:
-		for i in sub:
-			flat_words.append(i)
-	print(flat_words)
-	result  = len(flat_words)
-
-	return result                         # DO NOT CHANGE THIS LINE 
-#                          
-# NO CODE HERE
-#
-if __name__ == "__main__":              # DO NOT CHANGE THIS LINE 
-  print(word_counter('input1.txt'))     # DO NOT CHANGE THIS LINE 
-  print(word_counter('input2.txt'))     # DO NOT CHANGE THIS LINE 
-  print(word_counter('input3.txt'))     # DO NOT CHANGE THIS LINE 
-  print(word_counter('input4.txt'))     # DO NOT CHANGE THIS LINE 
-#                          
-# NO CODE HERE
-#
-def most_frequent(file_name):           # DO NOT CHANGE THIS LINE 
-  result1 = ''                          # DO NOT CHANGE THIS LINE 
-  result2 = 0                           # DO NOT CHANGE THIS LINE 
-    #
-    # Your code here
-    #
-  return result1, result2               # DO NOT CHANGE THIS LINE 
-#                          
-# NO CODE HERE
-#
-if __name__ == "__main__":              # DO NOT CHANGE THIS LINE 
-  print(most_frequent('input1.txt'))    # DO NOT CHANGE THIS LINE 
-  print(most_frequent('input2.txt'))    # DO NOT CHANGE THIS LINE 
-  print(most_frequent('input3.txt'))    # DO NOT CHANGE THIS LINE 
-  print(most_frequent('input4.txt'))    # DO NOT CHANGE THIS LINE 
-#                          
-# NO CODE HERE
-#
